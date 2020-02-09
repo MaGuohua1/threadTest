3f30354aec4e1187cebaa8d2b0044cb7efd71a98
MaGuohua1 <15834018326@yeah.net>
2020-02-09 15:56:54 +0800

    cap3

--- src/com/ma/cap3/C02_UseAtomicArray.java
+++ src/com/ma/cap3/C02_UseAtomicArray.java
@@ -7,10 +7,10 @@ public class C02_UseAtomicArray {
 	public static void main(String[] args) {
 		int[] arr = { 1, 2 };
 		AtomicIntegerArray array = new AtomicIntegerArray(arr);
-		
+
 		System.out.println(array.getAndAdd(0, 3));
 		System.out.println(array.getAndSet(1, 4));
-		
+
 		System.out.println(array.toString());
 	}
 }
